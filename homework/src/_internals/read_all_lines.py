06cf9ec47cfa6c24d15fee93d2512e8f0444e31a
MillerQuintero2001 <miquinterog@unal.edu.co>
2025-11-22 11:05:16 -0500

    Homework as package

--- homework/src/_internals/read_all_lines.py
+++ homework/src/_internals/read_all_lines.py
@@ -5,6 +5,6 @@ def read_all_lines(input_folder: str):
     input_file_list = os.listdir(input_folder)
     all_lines = []
     for filename in input_file_list:
-        with open(input_folder + filename, "r", encoding="utf-8") as f:
+        with open(os.path.join(input_folder, filename), "r", encoding="utf-8") as f:
             all_lines.extend(f.readlines())
     return all_lines
